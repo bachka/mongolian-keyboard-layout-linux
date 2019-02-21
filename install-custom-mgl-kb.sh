1c659dd7c2532aba1e3149cdc4d701d8b636e5df
bachka <bachka@intrafind.de>
2019-02-21 19:21:23 +0100

    increment ...

---
+++ install-custom-mgl-kb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# infos about paths https://michal.kosmulski.org/computing/articles/custom-keyboard-layouts-xkb.html
+
+sudo cp /usr/share/X11/xkb/symbols/mn /usr/share/X11/xkb/symbols/mn-backup
+sudo cp /home/bachka/repos-privat/mongolian-keyboard-layout-linux/mn-custom /usr/share/X11/xkb/symbols/mn
+# after all just restart system or logout
+
+# recover old one
+# sudo cp /usr/share/X11/xkb/symbols/mn-backup /usr/share/X11/xkb/symbols/mn
