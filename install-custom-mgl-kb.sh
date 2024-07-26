81ff1a3b4b1243e6e421490a11194fd4d4fff23a
bachka <tsengel.de@gmail.com>
2024-07-26 13:44:26 +0200

    add mongolian readme file

--- install-custom-mgl-kb.sh
+++ install-custom-mgl-kb.sh
@@ -2,7 +2,7 @@
 
 # infos about paths https://michal.kosmulski.org/computing/articles/custom-keyboard-layouts-xkb.html
 
-sudo cp /usr/share/X11/xkb/symbols/mn /usr/share/X11/xkb/symbols/mn-backup
+sudo cp /usr/share/X11/xkb/symbols/mn /usr/share/X11/xkb/symbols/mn-backup1
 sudo cp /home/bachka/repos-privat/mongolian-keyboard-layout-linux/mn-custom /usr/share/X11/xkb/symbols/mn
 # after all just restart system or logout
 
